d4c6a5e44b6d9f52301690925fa7ce495834c991
ShivMeena123 <128965199+ShivMeena123@users.noreply.github.com>
2024-01-14 00:57:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from './component/Header';
+import Footer from './component/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+
+function App() {
+  return (
+  
+      <BrowserRouter>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element = {<Home/>}/>
+        <Route path="contact/" element = {<Contact/>}/>
+        <Route path="about/" element = {<About/>}/>
+        <Route path="register/" element = {<Register/>}/>
+        <Route path="login/" element = {<Login/>}/>       
+        <Route path="services/" element = {<Services/>}/>       
+      </Routes>
+      <Footer></Footer>
+      </BrowserRouter>
+   
+  );
+}
+
+export default App;
